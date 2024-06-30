80b27832d9ed199a54f84bfe124d4273166b43ad
johnny <johnametepeagboku@gmail.com>
2024-06-30 13:55:44 +0000

    Update: Task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,14 +2,34 @@
 """
 The module contains
 """
+from typing import Any
+
 BaseCache = __import__('base_caching').BaseCache
+
 
 class FIFOCache(BaseCache):
     """
     The fifo cache
     """
 
-    def put(self, key: str, ):
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key: str, item: Any) -> None:
         """
-        
+        Add an item in the cache
         """
+
+        if key and item:
+            if len(self.cache_data) >= BaseCache.MAX_ITEMS:
+                discarded = self.queue.pop(0)
+                self.cache_data.remove(discarded)
+                print('DISCARD:', discarded)
+            self.cache_data[key] = item
+            self.queue.append(item)
+
+    def get(self, key: str) -> Any:
+        """Get an item by key"""
+
+        return self.cache_data.get(key, None)
